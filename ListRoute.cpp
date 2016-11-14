0f21423620e5b1afd6d451ba1f03ed1cf753aba7
gmarcelin <guillaume.marcelin@insa-lyon.fr>
2016-11-14 17:17:44 +0100

    Ajout ListRoute

--- ListRoute.cpp
+++ ListRoute.cpp
@@ -4,15 +4,22 @@
 
 #include "ListRoute.h"
 
-bool ListeRoute:addRoute(const Route* route){
+bool ListeRoute::addRoute(const Route* route){
 
 }
 
-ListRoute* ListeRoute:getDepartureFrom(const char* city){
+ListRoute* ListeRoute::getDepartureFrom(const char* city){
 
 }
 
+ListRoute* ListeRoute::getArrivalTo(const char* city){
 
+}
 
+size_t ListeRoute::getSize() const{
 
+}
 
+Route* ListeRoute::getElement(size_t i){
+
+};
